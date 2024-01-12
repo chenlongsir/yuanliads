c66bd19ae61a6feaba5eb3e99e0e8b4d0efbab8c
admin <2657450210@qq.com>
2024-01-12 17:01:39 +0800

    commit 1

---
+++ pangolinads/src/main/java/com/yuanli/ad/utils/RewardAdUtils.java
@@ -0,0 +1,153 @@
+package com.yuanli.ad.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.bytedance.sdk.openadsdk.AdSlot;
+import com.bytedance.sdk.openadsdk.TTAdConstant;
+import com.bytedance.sdk.openadsdk.TTAdNative;
+import com.bytedance.sdk.openadsdk.TTAdSdk;
+import com.bytedance.sdk.openadsdk.TTRewardVideoAd;
+import com.bytedance.sdk.openadsdk.mediation.ad.MediationAdSlot;
+import com.yuanli.ad.constants.AdConstants;
+import com.yuanli.ad.holder.TTAdManagerHolder;
+import com.yuanli.ad.listener.AdListener;
+import com.yuanli.ad.listener.AdStateListener;
+
+public class RewardAdUtils implements AdListener {
+    private Activity activity;
+    private TTRewardVideoAd mTTRewardVideoAd; // 插全屏广告对象
+    private TTAdNative.RewardVideoAdListener mRewardVideoListener; // 广告加载监听器
+    private TTRewardVideoAd.RewardAdInteractionListener mRewardVideoAdInteractionListener; // 广告展示监听器
+    private AdStateListener adStateListener;
+
+    public RewardAdUtils(Activity activity){
+        this.activity = activity;
+    }
+
+    private void initAdConfig(Context context) {
+        TTAdManagerHolder.get().requestPermissionIfNecessary(context);
+    }
+
+    public void loadAd(final AdStateListener adStateListener){
+        this.adStateListener = adStateListener;
+        /** 1、创建AdSlot对象 */
+        AdSlot adslot = new AdSlot.Builder()
+                .setCodeId(AdConstants.AD_REWARD_ID)
+                .setOrientation(TTAdConstant.ORIENTATION_VERTICAL)
+                .setMediationAdSlot(new MediationAdSlot
+                        .Builder()
+                        .setRewardName("rewardName")
+                        .setRewardAmount(100)
+                        .build())
+                .build();
+
+        /** 2、创建TTAdNative对象 */
+        TTAdNative adNativeLoader = TTAdSdk.getAdManager().createAdNative(activity);
+
+        /** 3、创建加载、展示监听器 */
+        initListeners();
+
+        adStateListener.loading();
+        /** 4、加载广告 */
+        adNativeLoader.loadRewardVideoAd(adslot, mRewardVideoListener);
+    }
+
+    private void initListeners() {
+        this.mRewardVideoListener = new TTAdNative.RewardVideoAdListener() {
+            @Override
+            public void onError(int i, String s) {
+                Log.i(AdConstants.TAG, "reward load fail: errCode: " + i + ", errMsg: " + s);
+                adStateListener.onError();
+            }
+
+            @Override
+            public void onRewardVideoAdLoad(TTRewardVideoAd ttRewardVideoAd) {
+                Log.i(AdConstants.TAG, "reward load success");
+                mTTRewardVideoAd = ttRewardVideoAd;
+                adStateListener.stopLoading();
+                adStateListener.real();
+            }
+
+            @Override
+            public void onRewardVideoCached() {
+                Log.i(AdConstants.TAG, "reward cached success");
+            }
+
+            @Override
+            public void onRewardVideoCached(TTRewardVideoAd ttRewardVideoAd) {
+                Log.i(AdConstants.TAG, "reward cached success 2");
+                mTTRewardVideoAd = ttRewardVideoAd;
+                adStateListener.stopLoading();
+                adStateListener.real();
+            }
+        };
+        this.mRewardVideoAdInteractionListener = new TTRewardVideoAd.RewardAdInteractionListener() {
+            @Override
+            public void onAdShow() {
+                Log.i(AdConstants.TAG, "reward show");
+            }
+
+            @Override
+            public void onAdVideoBarClick() {
+                Log.i(AdConstants.TAG, "reward click");
+            }
+
+            @Override
+            public void onAdClose() {
+                Log.i(AdConstants.TAG, "reward close");
+            }
+
+            @Override
+            public void onVideoComplete() {
+                Log.i(AdConstants.TAG, "reward onVideoComplete");
+            }
+
+            @Override
+            public void onVideoError() {
+                Log.i(AdConstants.TAG, "reward onVideoError");
+            }
+
+            @Override
+            public void onRewardVerify(boolean b, int i, String s, int i1, String s1) {
+                Log.i(AdConstants.TAG, "reward onRewardVerify");
+            }
+
+            @Override
+            public void onRewardArrived(boolean b, int i, Bundle bundle) {
+                Log.i(AdConstants.TAG, "reward onRewardArrived");
+                adStateListener.success();
+            }
+
+            @Override
+            public void onSkippedVideo() {
+                Log.i(AdConstants.TAG, "reward onSkippedVideo");
+            }
+        };
+    }
+
+
+    @Override
+    public void showAd() {
+        if (mTTRewardVideoAd == null) {
+            Log.i(AdConstants.TAG, "请先加载广告或等待广告加载完毕后再调用show方法");
+            return;
+        }
+        /** 5、设置展示监听器，展示广告 */
+        mTTRewardVideoAd.setRewardAdInteractionListener(mRewardVideoAdInteractionListener);
+        mTTRewardVideoAd.showRewardVideoAd(activity);
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mTTRewardVideoAd != null && mTTRewardVideoAd.getMediationManager() != null) {
+            mTTRewardVideoAd.getMediationManager().destroy();
+        }
+        mRewardVideoListener = null;
+        mRewardVideoAdInteractionListener = null;
+        adStateListener = null;
+        activity = null;
+    }
+}
---
+++ pangolinads/src/main/java/com/yuanli/ad/utils/SplashAdUtils.java
@@ -0,0 +1,189 @@
+package com.yuanli.ad.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.bytedance.sdk.openadsdk.AdSlot;
+import com.bytedance.sdk.openadsdk.TTAdConstant;
+import com.bytedance.sdk.openadsdk.TTAdNative;
+import com.bytedance.sdk.openadsdk.TTAppDownloadListener;
+import com.bytedance.sdk.openadsdk.TTSplashAd;
+import com.yuanli.ad.constants.AdConstants;
+import com.yuanli.ad.holder.TTAdManagerHolder;
+
+public class SplashAdUtils {
+    private static final String TAG = "SplashAdUtils";
+    private Context mContext;
+
+    /**
+     * 开屏广告加载超时时间,建议大于3000,这里为了冷启动第一次加载到广告并且展示,示例设置了3000ms
+     */
+    private static final int AD_TIME_OUT = 3500;
+
+    public void release() {
+        mContext = null;
+        mAdStateListener = null;
+    }
+
+    /**
+     * 获取广告位
+     */
+    private AdSlot getAdSlot() {
+        DisplayMetrics metrics = mContext.getResources().getDisplayMetrics();
+        int width = metrics.widthPixels;
+        int height = metrics.heightPixels;
+        return new AdSlot.Builder()
+                .setCodeId(AdConstants.AD_SPLASH_COD_ID)
+                .setSupportDeepLink(true)
+                .setImageAcceptedSize(width, height)
+                .setExpressViewAcceptedSize(width, height)
+                .build();
+    }
+
+    public void loadSplashAd(Context context, AdStateListener adStateListener) {
+        mContext = context;
+        mAdStateListener = adStateListener;
+        AdSlot adSlot = getAdSlot();
+        TTAdNative adNative = TTAdManagerHolder.get().createAdNative(context);
+        adNative.loadSplashAd(adSlot, splashAdListener, AD_TIME_OUT);
+    }
+
+    /**
+     * 开屏广告加载监听
+     */
+    private TTAdNative.SplashAdListener splashAdListener = new TTAdNative.SplashAdListener() {
+        @Override
+        public void onError(int code, String message) {
+            Log.e("开屏广告加载异常", message);
+            closeAd();
+        }
+
+        @Override
+        public void onTimeout() {
+            Log.e(TAG, "开屏广告加载超时");
+            closeAd();
+        }
+
+        @Override
+        public void onSplashAdLoad(TTSplashAd ttSplashAd) {
+            Log.d(TAG, "开屏广告请求成功");
+            if (ttSplashAd == null) {
+                closeAd();
+                return;
+            }
+            //获取SplashView
+            View view = ttSplashAd.getSplashView();
+            setSuccess(view);
+            //设置SplashView的交互监听器
+            setAdInteractionListener(ttSplashAd);
+            //设置SplashView的下载监听
+            if (ttSplashAd.getInteractionType() == TTAdConstant.INTERACTION_TYPE_DOWNLOAD) {
+                setDownloadListener(ttSplashAd);
+            }
+        }
+    };
+
+    /**
+     * SplashView的交互监听器
+     *
+     * @param ttSplashAd
+     */
+    private void setAdInteractionListener(TTSplashAd ttSplashAd) {
+        ttSplashAd.setSplashInteractionListener(new TTSplashAd.AdInteractionListener() {
+            @Override
+            public void onAdClicked(View view, int type) {
+                Log.d(TAG, "开屏广告点击");
+            }
+
+            @Override
+            public void onAdShow(View view, int type) {
+                Log.d(TAG, "开屏广告展示");
+            }
+
+            @Override
+            public void onAdSkip() {
+                Log.d(TAG, "开屏广告跳过");
+                closeAd();
+            }
+
+            @Override
+            public void onAdTimeOver() {
+                Log.d(TAG, "开屏广告倒计时结束");
+                closeAd();
+            }
+        });
+    }
+
+    /**
+     * 设置下载监听
+     *
+     * @param ttSplashAd
+     */
+    private void setDownloadListener(TTSplashAd ttSplashAd) {
+        ttSplashAd.setDownloadListener(new TTAppDownloadListener() {
+            boolean hasShow = false;
+
+            @Override
+            public void onIdle() {
+            }
+
+            @Override
+            public void onDownloadActive(long totalBytes, long currBytes, String fileName, String appName) {
+                if (!hasShow) {
+                    Toast.makeText(mContext, "下载中...", Toast.LENGTH_SHORT).show();
+                    hasShow = true;
+                }
+            }
+
+            @Override
+            public void onDownloadPaused(long totalBytes, long currBytes, String fileName, String appName) {
+                Toast.makeText(mContext, "下载暂停...", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDownloadFailed(long totalBytes, long currBytes, String fileName, String appName) {
+                Toast.makeText(mContext, "下载失败...", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDownloadFinished(long totalBytes, String fileName, String appName) {
+            }
+
+            @Override
+            public void onInstalled(String fileName, String appName) {
+            }
+        });
+    }
+
+    public interface AdStateListener {
+        /**
+         * 广告加载成功
+         *
+         * @param view
+         */
+        void onAdLoadSuccess(View view);
+
+        /**
+         * 广告关闭或异常
+         */
+        void onClose();
+    }
+
+    private AdStateListener mAdStateListener;
+
+    private void setSuccess(View view) {
+        if (mAdStateListener != null) {
+            mAdStateListener.onAdLoadSuccess(view);
+        }
+    }
+
+    private void closeAd() {
+        if (mAdStateListener != null) {
+            mAdStateListener.onClose();
+        }
+    }
+}
